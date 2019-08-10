4b1db87ae3ebd713f911391b3f864fbe0312d0df
Nathan Hawkes <nate@cocode.co.uk>
2019-08-10 23:03:57 +0100

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,7 +1,21 @@
 <?php get_header(); ?>
-
-	<h2>Error 404 - Page Not Found</h2>
-
-<?php get_sidebar(); ?>
-
+  <div id="container" class="pageContainer errorPage container">
+    <section class="mainContent" id="errorPage" <?php post_class() ?>>
+      <div class="postContent">
+        <div class="errorpage-icon">
+          <span class="fas fa-exclamation-triangle"></span>
+        </div>
+        <h1 class="page-title">Error 404 - Page Not found</h1>
+        <div class="entry">
+          <p>Seems the page you were looking for can’t be found on our site.  It’s either been deleted, the URL might be wrong, or you might have clicked on the wrong link.</p>
+          <p>Check the URL or use the menu above to try again.  You can also use the search form below.  If you still can’t find the page you’re looking for, get in touch with us and let us know which URL you are typing so we can fix it on our side.</p>
+        </div>
+        <div id="errorSearch">
+          <?php echo get_search_form(); ?>
+        </div>
+        <p class="errorpage-errorcode">HTTP_ERROR_404</p>
+      </div>
+    </section>
+    <?php // get_sidebar(); ?>
+  </div>
 <?php get_footer(); ?>
