0f24ad78126845de4b0396574111beca67f58955
Nathan Hawkes <nate@cocode.co.uk>
2019-08-12 23:57:53 +0100

    Daily Commit 2 20190812

--- inc/meta.php
+++ inc/meta.php
@@ -1,26 +1,26 @@
-<div class="post-meta clearfix">
-  <div class="post-meta-date">
-    <p><span class="fa fa-calendar"></span>
+<div class="postMeta clearfix">
+  <div class="postMeta-date metaElement">
+    <p><span class="fas fa-calendar postMeta-icon"></span>
       Posted on <a href="<?php the_permalink(); ?>"><?php echo get_the_date('F j, Y', '', ''); ?></a> by <?php the_author_posts_link(); ?></p>
   </div>
-  <div class="post-meta-comments">
+  <div class="postMeta-comments metaElement">
     <p>
       <?php if ( comments_open() ){ ?>
-      <span class="post-comments-enabled">
-        <span class="fa fa-comment"></span>
+      <span class="postComments-enabled">
+        <span class="fas fa-comment postMeta-icon"></span>
         <?php
           $number = (int) get_comments_number( get_the_ID() );
           if ( $number > 0 ){
-            $css_class = 'comments-link has-comment';
+            $css_class = 'commentsLink hasComment';
           } else {
-            $css_class = 'comments-link no-comment';
+            $css_class = 'commentsLink noComment';
           }
           comments_popup_link( 'Leave a comment', '1 comment', '% comments', '', '');
         ?>
       </span>
       <?php } else { ?>
-      <span class="post-comments-disabled">
-        <span class="fa fa-comment"></span>
+      <span class="postComments-disabled">
+        <span class="fas fa-comment postMeta-icon"></span>
         Comments disabled
       </span>
       <?php } ?>
--- search.php
+++ search.php
@@ -1,37 +1,28 @@
 <?php get_header(); ?>
-
-	<?php if (have_posts()) : ?>
-
-		<h2>Search Results</h2>
-
-		<?php include (TEMPLATEPATH . '/inc/nav.php' ); ?>
-
-		<?php while (have_posts()) : the_post(); ?>
-
-			<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-
-				<h2><?php the_title(); ?></h2>
-
-				<?php include (TEMPLATEPATH . '/inc/meta.php' ); ?>
-
-				<div class="entry">
-
-					<?php the_excerpt(); ?>
-
-				</div>
-
-			</div>
-
-		<?php endwhile; ?>
-
-		<?php include (TEMPLATEPATH . '/inc/nav.php' ); ?>
-
-	<?php else : ?>
-
-		<h2>No posts found.</h2>
-
-	<?php endif; ?>
-
-<?php get_sidebar(); ?>
-
+  <section id="pageTitle" class="archiveTitle pageTitle">
+    <div class="container">
+      <h1>Search Results</h1>
+    </div>
+  </section>
+  <div id="container" class="searchArchive pageContainer container">
+    <section class="mainContent" id="searchArchive">
+      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <article id="search-<?php the_ID(); ?>" <?php post_class() ?>>
+          <div class="postTitle">
+            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+          </div>
+          <div class="postPermalink">
+            <p><a href="<?php the_permalink(); ?>"><?php the_permalink(); ?></a></p>
+          </div>
+          <div class="postExcerpt"><?php echo wpautop( get_the_excerpt() ); ?></div>
+				  <?php include (TEMPLATEPATH . '/inc/meta.php' ); ?>
+        </article>
+      <?php endwhile; ?>
+        <?php zeroTheme_blogPagination(); ?>
+      <?php else : ?>
+        <h2>Not Found</h2>
+      <?php endif; ?>
+    </section>
+    <?php get_sidebar(); ?>
+  </div>
 <?php get_footer(); ?>
